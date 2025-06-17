c8bc5482c8abc3710f719ba75bc8d19fc50ab780
Mystical433 <peterpanlee2015@gmail.com>
2025-06-17 19:06:18 +0900

    Update ui.hpp

--- src/ui.hpp
+++ src/ui.hpp
@@ -4,55 +4,98 @@ using namespace geode::prelude;
 
 class MyPopup : public geode::Popup<> {
 protected:
-    CCLabelBMFont* m_label = nullptr;
-    CCLabelBMFont* m_descLabel = nullptr;
+    CCLabelBMFont* titleLabel;
+    CCLabelBMFont* contentLabel;
 
     bool setup() override {
-        this->setTitle("Welcome!");
+        this->setTitle("Python Tutorial");
 
-        m_descLabel = CCLabelBMFont::create("Useless mod.", "bigFont.fnt");
-        m_descLabel->setPosition({200, 180});
-        m_mainLayer->addChild(m_descLabel);
+        titleLabel = CCLabelBMFont::create("Select a Chapter", "bigFont.fnt");
+        titleLabel->setPosition({240.f, 270.f});
+        m_mainLayer->addChild(titleLabel);
 
-        m_label = CCLabelBMFont::create("Most useless thing", "bigFont.fnt");
-        m_label->setPosition({200, 130});
-        m_mainLayer->addChild(m_label);
+        contentLabel = CCLabelBMFont::create("", "goldFont.fnt", 400.f, kCCTextAlignmentLeft);
+        contentLabel->setPosition({240.f, 160.f});
+        contentLabel->limitLabelWidth(400.f, 1.f, 0.1f);
+        m_mainLayer->addChild(contentLabel);
 
         auto menu = CCMenu::create();
         menu->setPosition({0, 0});
+
+        for (int i = 1; i <= 10; ++i) {
+            auto label = CCLabelBMFont::create(("Ch. " + std::to_string(i)).c_str(), "bigFont.fnt");
+            auto button = CCMenuItemLabel::create(label, this, menu_selector(PythonPopup::onChapter));
+            button->setTag(i);
+            float x = 60.f + ((i - 1) % 5) * 90.f;
+            float y = 70.f - ((i - 1) / 5) * 50.f;
+            button->setPosition({x, y});
+            menu->addChild(button);
+        }
+
+        // Close button
+        auto closeLabel = CCLabelBMFont::create("Close", "bigFont.fnt");
+        auto closeButton = CCMenuItemLabel::create(closeLabel, this, menu_selector(PythonPopup::onClose));
+        closeButton->setPosition({440.f, 30.f});
+        menu->addChild(closeButton);
+
         m_mainLayer->addChild(menu);
-
-        auto buttonSprite = ButtonSprite::create(" Useless Mod ");
-        buttonSprite->setScale(0.9f);
-        buttonSprite->setZOrder(0);
-        buttonSprite->setContentSize({150.f, buttonSprite->getContentSize().height});
-
-        auto button = CCMenuItemSpriteExtra::create(
-            buttonSprite,
-            buttonSprite,
-            this,
-            menu_selector(MyPopup::onButtonClick)
-        );
-        button->setPosition({20, 200}); // top left corner inside 400x240 popup
-        menu->addChild(button);
-
         return true;
     }
 
-    void onButtonClick(CCObject*) {
-        this->setTitle("Useless Mod");
-        if (m_label) {
-            m_label->setString("Useless Mod");
-        }
-        if (m_descLabel) {
-            m_descLabel->setString("die");
+    void onChapter(CCObject* sender) {
+        int chapter = static_cast<CCNode*>(sender)->getTag();
+        switch (chapter) {
+            case 1:
+                titleLabel->setString("Chapter 1: Variables");
+                contentLabel->setString("Variables store data:\nx = 5\ny = 'Hello'");
+                break;
+            case 2:
+                titleLabel->setString("Chapter 2: Data Types");
+                contentLabel->setString("Common types:\nint, float, str, list, dict");
+                break;
+            case 3:
+                titleLabel->setString("Chapter 3: Control Flow");
+                contentLabel->setString("if, else, for, while:\nif x > 5:\n  print(x)");
+                break;
+            case 4:
+                titleLabel->setString("Chapter 4: Functions");
+                contentLabel->setString("def greet(name):\n  return 'Hi ' + name");
+                break;
+            case 5:
+                titleLabel->setString("Chapter 5: Lists");
+                contentLabel->setString("fruits = ['apple', 'banana']\nprint(fruits[0])");
+                break;
+            case 6:
+                titleLabel->setString("Chapter 6: Dictionaries");
+                contentLabel->setString("person = {'name': 'Alice', 'age': 30}\nprint(person['name'])");
+                break;
+            case 7:
+                titleLabel->setString("Chapter 7: Classes");
+                contentLabel->setString("class Dog:\n  def bark(self):\n    print('Woof!')");
+                break;
+            case 8:
+                titleLabel->setString("Chapter 8: Modules");
+                contentLabel->setString("import math\nprint(math.sqrt(16))");
+                break;
+            case 9:
+                titleLabel->setString("Chapter 9: File I/O");
+                contentLabel->setString("with open('file.txt') as f:\n  data = f.read()");
+                break;
+            case 10:
+                titleLabel->setString("Chapter 10: Exceptions");
+                contentLabel->setString("try:\n  x = 1 / 0\nexcept ZeroDivisionError:\n  print('Error')");
+                break;
         }
     }
 
+    void onClose(CCObject*) {
+        this->dismiss();
+    }
+
 public:
-    static MyPopup* create() {
-        auto ret = new MyPopup();
-        if (ret->initAnchored(400.f, 240.f)) {
+    static PythonPopup* create() {
+        auto ret = new PythonPopup();
+        if (ret->initAnchored(480.f, 320.f)) {
             ret->autorelease();
             return ret;
         }
