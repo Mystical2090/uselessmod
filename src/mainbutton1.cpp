6e5d02fef143f0de41ab6b3b93359ea1a6e0d26b
Mystical433 <peterpanlee2015@gmail.com>
2025-07-06 21:53:51 +0900

    Update mainbutton1.cpp

--- src/mainbutton1.cpp
+++ src/mainbutton1.cpp
@@ -1,5 +1,28 @@
 #include <Geode/Geode.hpp>
 #include "pythonui.hpp"
+#include <Geode/Geode.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/EditLevelLayer.hpp>
+
+class $modify(LevelInfoLayer) {
+    bool init(GJGameLevel* level, bool challenge) {
+        if (!LevelInfoLayer::init(level, challenge))
+            return false;
+        
+        m_level->m_lowDetailModeToggled = true;
+        return true;
+    }
+};
+
+class $modify(EditLevelLayer) {
+    bool init(GJGameLevel* level) {
+        if (!EditLevelLayer::init(level))
+            return false;
+        
+        m_level->m_lowDetailModeToggled = true;
+        return true;
+    }
+};
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
